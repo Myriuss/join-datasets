fb76cf2a560187efaa4c4803e34e5e1f2dfa10fe
Myriuss <marielelyamani@gmail.com>
2024-05-18 11:29:45 +0200

    :bug: Bug fixed

--- jointure.py
+++ jointure.py
@@ -8,8 +8,8 @@ df_2022 = pd.read_csv('cleanfinal_pres2022.csv', encoding='ISO-8859-1')
 df_2017['codegeo'] = df_2017['codegeo'].apply(lambda x: x.zfill(2))
 df_2022['codegeo'] = df_2022['codegeo'].apply(lambda x: x.zfill(2))
 
-# Effectuer la jointure sur la colonne 'codegeo'
-df_merged = pd.merge(df_2017, df_2022, on='codegeo', suffixes=('_2017', '_2022'))
+# Effectuer la jointure sur les colonnes 'codegeo' et 'libelle_departement'
+df_merged = pd.merge(df_2017, df_2022, on=['codegeo', 'libelle_departement'], suffixes=('_2017', '_2022'))
 
 # Écrire le résultat dans un nouveau fichier CSV
 df_merged.to_csv('jointure.csv', index=False, encoding='utf-8')
