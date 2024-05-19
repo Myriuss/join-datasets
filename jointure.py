c80b90163dc8746d7b247139ab64d946345efa7b
Myriuss <marielelyamani@gmail.com>
2024-05-19 12:56:20 +0200

    :white_check_mark: Dataset emploi added

--- jointure.py
+++ jointure.py
@@ -5,11 +5,13 @@ chomage_df = pd.read_csv("chomageNet.csv", sep=";", encoding='ISO-8859-1')
 pres2017_df = pd.read_csv("cleanfinal_pres2017.csv", sep=";", encoding='ISO-8859-1')
 pres2022_df = pd.read_csv("cleanfinal_pres2022.csv", sep=";", encoding='ISO-8859-1')
 population_df = pd.read_csv("CleanPopulation1.csv", sep=";", encoding='ISO-8859-1')
+emploi_df = pd.read_csv("emploi_clean.csv", sep=";", encoding='ISO-8859-1')
 
 # Effectuer la jointure entre les DataFrames
 result = chomage_df.merge(pres2017_df, on=["codegeo", "Libelle_departement"], how="inner") \
                    .merge(pres2022_df, on=["codegeo", "Libelle_departement"], how="inner") \
-                   .merge(population_df, on=["codegeo", "Libelle_departement"], how="inner")
+                   .merge(population_df, on=["codegeo", "Libelle_departement"], how="inner") \
+                   .merge(emploi_df, on=["codegeo", "Libelle_departement"], how="inner")
 
 # Afficher un aperçu du résultat
 print(result.head())
